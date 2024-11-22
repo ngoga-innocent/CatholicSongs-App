7260a1aed899366cae9ac080a81598ca2d6043f9
CSTIM COMP LAB <ngogainnocent1@gmail.com>
2024-11-22 14:51:23 +0200

    some changes

--- Url.js
+++ Url.js
@@ -1,2 +1,2 @@
-export const Url='https://308e-105-178-104-150.ngrok-free.app'
-// export const Url='https://catholicsong-d02m.onrender.com'
+// export const Url='https://308e-105-178-104-150.ngrok-free.app'
+export const Url='https://catholicsong-d02m.onrender.com'
